dcea5851880fba473a3d23258dc4556ed37f4ad5
one0910 <one0910.yu@msa.hinet.net>
2023-09-20 14:56:37 +0800

    [增加] Logout API

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,6 +1,7 @@
 import React, { useContext, useState, Dispatch, SetStateAction } from 'react'
 import { OrderContext } from '../store'
 import { useNavigate } from 'react-router-dom';
+import { authFetch } from '../utilities';
 
 interface LogoutProps {
 	isLogin: boolean
@@ -13,8 +14,9 @@ export const Logout: React.FC<LogoutProps> = ({ isLogin, setIsLogin }) => {
 
 	const clickHandler = () => {
 		localStorage.removeItem("userToken")
-		setIsLogin(false)
 		document.cookie = "remember_me=; path=/; SameSite=None; Secure; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+		authFetch.get('/api/google/logout')
+		setIsLogin(false)
 		if (isLogin || state.orderList.status === "member") {
 			dispatch({
 				type: "CLEAR_ORDER",
@@ -26,6 +28,7 @@ export const Logout: React.FC<LogoutProps> = ({ isLogin, setIsLogin }) => {
 			})
 			navigate("/")
 		}
+
 	}
 	return (
 		<button className="btn btn-sm btn-outline-warning me-2" type="button" onClick={clickHandler}>
